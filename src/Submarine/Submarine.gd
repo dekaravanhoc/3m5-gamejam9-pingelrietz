2e961ead6cfc0751ca37776961b0b5376955ee6c
Dekara VanHoc <vanhocgames@gmail.com>
2022-10-29 15:29:19 +0200

    changes sub movement

--- src/Submarine/Submarine.gd
+++ src/Submarine/Submarine.gd
@@ -7,7 +7,7 @@ var current_state = States.FREE
 var gold : int
 export (float) var max_fuel: float = 100
 var current_fuel : float 
-export (float) var fuel_loss_rate: float = 0.1
+export (float) var fuel_loss_rate: float = 1
 
 #body in range to interact with
 var body
@@ -23,10 +23,7 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	#Movement
 	if is_moving() and current_fuel > 0:
-		movement_vector = get_input_direction()
-		current_fuel -= fuel_loss_rate
-	else:
-		movement_vector = Vector2.ZERO
+		current_fuel = clamp(current_fuel - fuel_loss_rate * delta, 0, max_fuel)
 	#Interaction
 	if current_state != States.FREE && Input.is_action_just_pressed("interact"):
 		if current_state == States.STEAL && body is EnemyShip:
@@ -38,6 +35,9 @@ func _physics_process(delta: float) -> void:
 			pass
 
 func is_moving():
+	return movement_vector != Vector2.ZERO
+
+func is_controller_input():
 	return Input.is_action_pressed("move_down") or Input.is_action_pressed("move_up") or Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right")
 
 func get_input_direction():
@@ -45,12 +45,26 @@ func get_input_direction():
 		Input.get_action_raw_strength("move_right") - Input.get_action_raw_strength("move_left"), 
 		Input.get_action_raw_strength("move_down") - Input.get_action_raw_strength("move_up")
 	)
+	print( Input.get_joy_name(0))
+	if Input.get_joy_name(0) == "":
+		print("s")
+	
 
 func spotted():
 	print("spotted!")
 
 func gain_gold():
 	gold += body._steal()
+	
+	
+func _input(event):
+	if is_controller_input():
+		movement_vector = Vector2(
+		Input.get_action_raw_strength("move_right") - Input.get_action_raw_strength("move_left"), 
+		Input.get_action_raw_strength("move_down") - Input.get_action_raw_strength("move_up")
+		)
+	else:
+		movement_vector = Vector2.ZERO
 
 
 func _on_Area2D_body_entered(body: Node) -> void:
