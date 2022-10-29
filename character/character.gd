5911ed4d8047e20268ae87666fbb48125efa7b10
Dekara VanHoc <vanhocgames@gmail.com>
2022-10-29 11:04:24 +0200

    adds character class

---
+++ character/character.gd
@@ -0,0 +1,13 @@
+class_name Character
+extends KinematicBody2D
+
+export (int) var max_speed: int = 100
+export (int) var acceleration: int = 10
+
+var movement_vector: Vector2 = Vector2.ZERO
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+	pass
