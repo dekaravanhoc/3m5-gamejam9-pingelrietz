9636ba826d8fec52054f9f4a90d141c82107f227
James <jamison.bassett@gmail.com>
2022-10-30 17:41:49 +0100

    adds radar colors for enemies and ramschladen

--- src/HUD/radar/radar.gd
+++ src/HUD/radar/radar.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
-var color := ColorN("green")
+export (Color) var lines_color := ColorN("green")
+export (Color) var enemy_color := ColorN("red")
+export (Color) var ramschladen_color := ColorN("cyan")
 var line_width := 3
 var point_radius := 6
 
@@ -24,17 +26,21 @@ func _ready():
 
 func _draw():
 	
-	draw_arc(radar_center, 150, 0, PI*2, 100, color, line_width)
-	draw_arc(radar_center, 100, 0, PI*2, 100, color, line_width)
-	draw_arc(radar_center, 50, 0, PI*2, 100, color, line_width)
-	draw_arc(radar_center, point_radius, 0, PI*2, 100, color, line_width)
+	draw_arc(radar_center, 150, 0, PI*2, 100, lines_color, line_width)
+	draw_arc(radar_center, 100, 0, PI*2, 100, lines_color, line_width)
+	draw_arc(radar_center, 50, 0, PI*2, 100, lines_color, line_width)
+	draw_arc(radar_center, point_radius, 0, PI*2, 100, lines_color, line_width)
 	
 	var pois := get_tree().get_nodes_in_group("poi")
 		
 	for poi in pois:
 		var local_position: Vector2 = poi.global_position - Game.submarine.global_position
 		var radar_position: Vector2 = local_position / radar_scale
-			
+		var color = lines_color
+		if poi is EnemyShip:
+			color = enemy_color
+		if poi is Ramschladen:
+			color = ramschladen_color
 		if radar_position.length() <= radar_size:
 			draw_circle(radar_position + radar_center, point_radius, color)
 
